4126b3a8b3c17eec4c4aaaeb4ca958000c523994
gldev <gldev@xyz.com>
2020-05-01 18:09:48 -0500

    handle memory

--- pyglet-cornway.py
+++ pyglet-cornway.py
@@ -3,7 +3,7 @@ import sys, numpy
 
 window = pyglet.window.Window(500, 500)
 
-cell_width, cell_height = 10, 10
+cell_width, cell_height = 20, 20
 
 pixel_width, pixel_height = int(500/cell_width), int(500/cell_height)
 
@@ -12,6 +12,8 @@ bat = pyglet.graphics.Batch()
 
 white = [255]*4
 black = [0]*4
+
+mem_cache = {}
 
 state = numpy.zeros(( cell_width, cell_height ))
 
@@ -34,15 +36,14 @@ def on_draw():
 
 def update(dt):
     global state
-    vertex_list = pyglet.graphics.vertex_list(4, 'v2f', 'c3B')
     statecpy = numpy.copy(state)
     for y in range(0, cell_height):
         for x in range(0, cell_width):
             vertex = [
                 (x)* pixel_width, (y+1) * pixel_height,#top left
                 (x+1) * pixel_width, (y + 1) * pixel_height,#top right
-                (x+1) * pixel_width, y,#bottom right
-                x * pixel_width, y,#bottom left 
+                (x+1) * pixel_width, y*pixel_height,#bottom right
+                x * pixel_width, y*pixel_height,#bottom left 
             ]
             n = neigh(x, y, state, cell_width, cell_height)
             
@@ -51,13 +52,18 @@ def update(dt):
             elif state[x, y] == 1 and (n < 2 or n > 3):
                 statecpy[x, y] = 0
 
-            if statecpy[x, y] == 0:
-                batch.add(4, pyglet.gl.GL_QUADS, None, ('v2i',vertex), ('c4B', white*4))
-                #batch.add_indexed(4, pyglet.gl.GL_QUADS, None, [0, 1, 2, 3, 4], ('c4B', white*4))
+            square = mem_cache.get((y, x))
+            color = ('c4B', white*4)
+            if square:
+                if statecpy[x, y] != 0:
+                    square.colors = black*4
+                else:
+                    square.colors = white*4
             else:
-                batch.add(4, pyglet.gl.GL_QUADS, None, ('v2i',vertex), ('c4B', black*4))
-                #batch.add_indexed(4, pyglet.gl.GL_QUADS, None, [0, 1, 2, 3, 4], ('c4B', black*4))
-            vertex_list.vertices = vertex
+                if statecpy[x, y] != 0:
+                    color = ('c4B', black*4)
+                
+                mem_cache[y,x] = batch.add(4, pyglet.gl.GL_QUADS, None, ('v2i',vertex), color)
     state = numpy.copy(statecpy)
 
 if __name__ == "__main__":
